3d46607373160aab8830a9b0f3ce05167498ac41
Michael Kabutey <billyston@Forges-MacBook-Pro.local>
2023-04-19 23:13:18 -0700

    Route: API versioning done

--- routes/api.php
+++ routes/api.php
@@ -1,16 +1,12 @@
 <?php
 
-use Illuminate\Http\Request;
+declare(strict_types=1);
+
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
+/**
+ * Version 1
+ */
+Route::prefix('v1')->as('v1:')->group(
+    base_path('routes/v1/api.php'),
+);
