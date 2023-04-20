c652b89ba0150ce15cab16cd874acb1dc46a8779
Michael Kabutey <billyston@Forges-MacBook-Pro.local>
2023-04-20 00:31:33 -0700

    Ping route created and used. Pending response structure

--- app/Http/Controllers/V1/Ping/PingController.php
+++ app/Http/Controllers/V1/Ping/PingController.php
@@ -4,13 +4,12 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers\V1\Ping;
 
-use Illuminate\Contracts\Support\Responsable;
 use Illuminate\Http\Request;
 
 final class PingController
 {
-    public function __invoke( Request $request ) : Responsable
+    public function __invoke( Request $request ) : bool
     {
-        //
+        return true;
     }
 }
