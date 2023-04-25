255116f72886fde8503862ede9f0bab80a43b739
Michael Kabutey <billyston@Forges-MacBook-Pro.local>
2023-04-25 10:05:04 -0700

    Basic exception handler configurations to return JsonResponse done

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -1,12 +1,28 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Exceptions;
 
+use App\Traits\v1\apiResponseBuilder;
+use Exception;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\Eloquent\RelationNotFoundException;
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Exceptions\ThrottleRequestsException;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
 {
+    use apiResponseBuilder;
+
     /**
      * A list of exception types with their corresponding custom log levels.
      *
@@ -35,6 +51,35 @@ class Handler extends ExceptionHandler
         'password',
         'password_confirmation',
     ];
+
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * @param Request $request
+     * @param Exception $exception
+     * @return Response|JsonResponse
+     * @throws Exception|Throwable
+     */
+    public function render($request, $exception): Response|JsonResponse
+    {
+        if ($exception instanceof ModelNotFoundException && $request->wantsJson()) {
+            return $this->responseBuilder(false, Response::HTTP_NOT_FOUND, 'Resource not found.');
+        } elseif ($exception instanceof NotFoundHttpException) {
+            return $this->responseBuilder(false, Response::HTTP_NOT_FOUND, 'Route not found.');
+        } elseif ($exception instanceof MethodNotAllowedHttpException) {
+            return $this->responseBuilder(false, Response::HTTP_METHOD_NOT_ALLOWED, 'You are not allowed to perform this action.');
+        } elseif ($exception instanceof QueryException) {
+            return $this->responseBuilder(false, Response::HTTP_UNAUTHORIZED, 'Invalid database query.');
+        } elseif ($exception instanceof RelationNotFoundException) {
+            return $this->responseBuilder(false, Response::HTTP_INTERNAL_SERVER_ERROR, 'Undefined relationship.');
+        } elseif ($exception instanceof AuthenticationException) {
+            return $this->responseBuilder(false, Response::HTTP_UNAUTHORIZED, 'User not authenticated.');
+        } elseif ($exception instanceof ThrottleRequestsException) {
+            return $this->responseBuilder(false, Response::HTTP_TOO_MANY_REQUESTS, 'Too many requests.');
+        }
+
+        return parent::render($request, $exception);
+    }
 
     /**
      * Register the exception handling callbacks for the application.
